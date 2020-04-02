75cd698638e93c015a917538ea396cea7d0709b7
好奇的 01 <hizyx@foxmail.com>
2020-04-02 14:05:45 +0800

    Add empty manage-aphorisms.

---
+++ manage-aphorisms.php
@@ -0,0 +1,34 @@
+<?php
+if (!defined('__TYPECHO_ADMIN__')) exit;
+
+include 'header.php';
+include 'menu.php';
+?>
+
+<div class="main">
+    <div class="body container">
+        <?php include 'page-title.php'; ?>
+        <div class="row typecho-page-main" role="main">
+               <div class="col-mb-12 typecho-list">
+                <div class="typecho-table-wrap">
+                    <table class="typecho-list-table">
+                        <colgroup>
+                        </colgroup>
+                        <thead>
+                            <tr>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table><!-- end .typecho-list-table -->
+                </div><!-- end .typecho-table-wrap -->
+            </div><!-- end .typecho-list -->
+        </div><!-- end .typecho-page-main -->    </div>
+</div>
+
+<?php
+include 'copyright.php';
+include 'common-js.php';
+?>
+
+<?php include 'footer.php'; ?>
