eb9ce73486d484d3cfcfa98432888c507a946e87
好奇的 01 <hizyx@foxmail.com>
2020-04-13 23:38:30 +0800

    Add Widget/Edit.php

---
+++ Widget/Edit.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+/**
+ * 名言警句
+ * @author 好奇的 01
+ */
+class Aphorisms_Widget_Edit extends Aphorisms_Widget implements Widget_Interface_Do
+{
+    /**
+     * action 入口函数
+     *
+     * @access public
+     * @return void
+     */
+    public function action()
+    {
+    }
+}
